34b5f5d890e9681643146bf6eda9696c5c7a0d32
Varsha Bajaj <varshabajaj@Varshas-MacBook-Air.local>
2025-05-10 17:38:01 +0530

    adding initial

---
+++ FirstProgram.py
@@ -0,0 +1,13 @@
+import logging
+logging.basicConfig(level=logging.DEBUG)
+logging.info("lets start")
+given_number = int(input("enter the number:"))
+result = given_number%2
+if result == 0:
+    print("number is even")
+    if result =x`x` 0:
+        print("valid")
+    else:
+        print("invalid")
+else:
+    print("number is odd")
